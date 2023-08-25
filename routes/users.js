ff1e6b0be9c1a0826f63599e47002aedf7bda6a3
Demilade Onasanya <demigonas@gmail.com>
2023-08-25 15:02:31 -0400

    Completed files

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  res.send(users);
+  res.send(JSON.stringify({users},null,4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
@@ -35,6 +35,27 @@ router.get("/:email",(req,res)=>{
   res.send(filtered_users);
 });
 
+// GET users with a particular Last Name eg. 'Smith'
+router.get("/lastName/:lastName",(req,res)=>{
+    const lastName = req.params.lastName;
+    let filtered_lastname = users.filter((user) => user.lastName === lastName);
+    res.send(filtered_lastname);
+});
+
+function getDateFromString(strDate) {
+    let [dd,mm,yyyy] = strDate.split('-')
+    return new Date(yyyy+"/"+mm+"/"+dd);
+}
+    
+// console.log(sorted_users);
+router.get("/sort",(req,res)=>{
+    let sorted_users=users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+            return d1-d2;
+          });
+    res.send(sorted_users);
+});
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
@@ -61,10 +82,10 @@ router.put("/:email", (req, res) => {
         if(DOB) {
             filtered_user.DOB = DOB
         }
-        if(DOB) {
+        if(lastName) {
             filtered_user.lastName = lastName
         }
-        if(DOB) {
+        if(firstName) {
             filtered_user.firstName = firstName
         }
 
@@ -80,8 +101,9 @@ router.put("/:email", (req, res) => {
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  users = users.filter((user) => user.email != email);
+  res.send(`User with the email ${email} deleted.`)//This line is to be replaced with actual return value
 });
 
 module.exports=router;
